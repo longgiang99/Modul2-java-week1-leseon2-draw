f0cd30badd79adffeebfb56327138f51d540cbc0
trieu <longgiang9999.gl@gmail.com>
2023-03-24 14:02:48 +0700

    ve hinh theo men nu da chon

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("chon hinh:");
+            System.out.println("3. hinh tam giac");
+            System.out.println("2. hinh chu nhat");
+            System.out.println("0. exit");
+            System.out.println(" enter your choice");
+            choice = input.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println(" tam giac");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("vuong ");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("chu nhat");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+    }
+}
